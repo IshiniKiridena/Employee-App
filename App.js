03b8b52c1c2a6a3d7aef120c5b8b1ded2ef02451
Ishini Kiridena <ishini.kiridenaedu@gmail.com>
2022-12-11 13:17:05 +0530

    employee add completed

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "./components/Home";
 import EmployeeView from "./components/Employees";
+import AddEmployee from "./components/AddEmployee";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,10 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Employees" component={EmployeeView} />
+        <Stack.Screen
+          name="Add Employee"
+          component={AddEmployee}
+        ></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
